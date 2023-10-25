1f2ffe9fe5faf13c8ee337ccb881efca677ad057
70ash <1486515335@qq.com>
2023-10-25 23:14:24 +0800

    "反转链表"

---
+++ src/链表/ListNode.java
@@ -0,0 +1,13 @@
+package 链表;
+
+/**
+ * @author zlp
+ * @date 2023/10/22 17:46
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
---
+++ src/链表/ReverseLinkedList.java
@@ -0,0 +1,19 @@
+package 链表;
+
+/**
+ * @author zlp
+ * @date 2023/10/22 17:43
+ * 206.反转链表
+ * https://leetcode.cn/problems/reverse-linked-list/description/
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ */
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+
+    }
+    public ListNode reverseList(ListNode head) {
+        ListNode next;
+        ListNode cur;
+        return null;
+    }
+}
